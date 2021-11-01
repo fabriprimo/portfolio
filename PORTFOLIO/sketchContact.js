578f625d420bb663233f2462c0db2e8a296ab7ad
fabriprimo <84012889+fabriprimo@users.noreply.github.com>
2021-11-01 17:49:22 +0100

    Update sketchContact.js

--- PORTFOLIO/sketchContact.js
+++ PORTFOLIO/sketchContact.js
@@ -4,24 +4,32 @@ function windowResized(){
 }
 function setup(){
   
-   canvas = createCanvas(windowWidth, windowHeight);
+   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
-    frameRate(10);
+    
 
 
 }
 
 function draw(){
 
-    background(0);
-    noStroke();
-let fx = map(mouseX, 0, width, 0, 255);
+  var dirX = map(mouseX,0,width,1,-1);
+  var dirY = map(mouseY,0,height,1,-1);
+  var dirZ = -0.2;
 
-for (let i=0; i<10; i++){
-    fill(fx);
-    ellipse(random(width), random(height), i*20, i*20);
-
-}
+  background(0);
+  fill(255);
+  noStroke();
+  directionalLight(255, 255, 255, dirX, dirY, dirZ);
+  push();
+  translate(width/2, height/2);
+  //rotateY(radians(frameCount));
+  //rotateX(radians(-frameCount));
+  //rotateZ(radians(frameCount));
+  //box(400, 100, 100);
+  sphere(200);
+  pop();
+  
 }
 
