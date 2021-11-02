ec68de62b494fc9d70ef1b60ae71ea5345d32e18
fabriprimo <84012889+fabriprimo@users.noreply.github.com>
2021-11-02 01:12:39 +0100

    Update sketchHome2.js

--- PORTFOLIO/sketchHome2.js
+++ PORTFOLIO/sketchHome2.js
@@ -1,25 +1,118 @@
-let moon;
-
-function preload() {
-  // Load model with normalise parameter set to true
-  moon = loadModel('assets/Moon.obj', true);
-}
-
-function windowResized(){
-    
-    resizeCanvas(windowWidth, windowHeight);
-}
-function setup(){
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 2;
   
-    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-    canvas.position(0, 0);
-    canvas.style('z-index', '-1');
+  this.prevPos = this.pos.copy();
+  
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+  
+  this.follow = function(vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  }
+  
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  
+  this.show = function() {
+	stroke(255, 5);
+    strokeWeight(3);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+  
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  } 
+}
+
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
+
+function setup() {
+  createCanvas(1600, 800);
+  cols = floor(width/scl);
+  rows = floor(height/scl);
+  fr = createP('');
+  
+  flowfield = new Array(cols * rows);
+  
+  for (var i = 0; i < 2500; i++) {
+  	particles[i] = new Particle();
+  }
+  background(0);
+} 
+
 
 function draw() {
-  background(200);
-  scale(1); // Scaled to make model fit into canvas
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  normalMaterial(); // For effect
-  model(moon);
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+  	for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 2;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(255, 50);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
+  	}
+    yoff += inc;
+    
+    zoff += 0.0003;
+  }
+  
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+  	particles[i].update();
+    particles[i].edges();
+  	particles[i].show();
+  }
+  
+  fr.html(floor(frameRate()));
 }
