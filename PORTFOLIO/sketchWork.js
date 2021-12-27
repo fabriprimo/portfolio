22f5b5d24acad3520ff5634484a205a9a196d8a0
fabriprimo <84012889+fabriprimo@users.noreply.github.com>
2021-12-27 23:34:22 +0100

    Delete PORTFOLIO directory

--- PORTFOLIO/sketchWork.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("myVid").addEventListener("mouseover", function() {
-	this.play();
-});
-
-document.getElementById("myVid").addEventListener("mouseleave", function() {
-	this.pause();
-});
